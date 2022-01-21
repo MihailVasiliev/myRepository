781346909c53503256d88c19427d8797ee2d4a39
Michail <m.a.vasiliev@mail.ru>
2022-01-21 22:06:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+//'use strict';
+
+// a = 5;
+// console.log(a);
+
+// переменные
+
+//console.log(a);
+// let a = 1; // создаётся в момент исполнения кода
+// console.log(a);
+// const b = 3.14;
+//b = 4;
+// a = 6;
+// console.log(a);
+// {
+//     let c = 99;
+// }
+//console.log(c);
+// a = 'hello world';
+// console.log(a);
+
+
+//типы данных
+
+// let number = 5;
+// let string = 'hello';
+// let sym = Symbol();
+// let bool = true;
+ //let n = null;
+// let undef; //underfined;
+// let obj = {};
+// console.log(4/0); //infinity числовое значение
+// console.log('str'/5); //NaN числовое значение
+//console.log(n);
+
+
+//  обычные объекты
+
+// let persone = {
+//     name: 'Bob',
+//     age: 30,
+//     isMarried: false
+// };
+// console.log(persone.age);
+// console.log(persone["age"]);
+
+
+// специальные объекты (со) массивы 
+
+// let arr = ['apple', 45, true];
+// console.log(arr[0]);
+
+
+// (ос): arrays; function; object data; regular expressions; errors.
+
+
+//модальные окна
+
+// alert('hello world'); //выполнение скриптов останавливается
+// let answer = confirm('are you agree?');
+// console.log(answer);    
+// let answer2 = prompt('are you 18?', 'yes');
+// console.log(answer2);
+// console.log(typeof(answer2));
+//официальная ошибка с null
+// console.log(typeof(null));
+
+
+// оператор + 
+// конкотинация
+
+// console.log('hello' + ' ' + 'world');
+// console.log(5 + ' apples');
+
+
+
+// явное преобразование
+
+// let answer3 = +prompt('are you 18?', 'yes');  //вводим число потом микс
+// console.log(answer3);
+// console.log(typeof(answer3));
+
+//-------------------------------------------
+
+// унарный оператор + инкримент и - дикримент префиксная форма и постфиксная
+
+// let incr = 10, dicr = 10;
+// console.log(++incr);
+// console.log(--dicr);
+// console.log(incr++);
+// console.log(dicr--);
+// console.log(incr);
+// console.log(dicr);
+
+//-------------------------------------
+
+// операторы + - / * %
+
+// console.log(5%2);
+
+//------------------------------------
+
+// операторы == === 
+
+// console.log('2' == 2);
+// console.log('2' === 2); 
+
+//--------------------------------------
+
+// && || ! !=
+
